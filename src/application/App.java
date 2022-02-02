7fad6414e5b0657c86cab70ad8f8330020bd6bb4
earl-stephens <stephensearl@hotmail.com>
2022-02-01 23:01:27 -0700

    Create example of abstract class

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,26 @@
 package application;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(new Task(), 0, 1000);
 	}
 
 }
+
+//can have more than one class in a file, but only one of them
+//is the public class
+class Task extends TimerTask {
+
+	@Override
+	public void run() {
+		System.out.println("hello");
+		
+	}
+	
+}
+
